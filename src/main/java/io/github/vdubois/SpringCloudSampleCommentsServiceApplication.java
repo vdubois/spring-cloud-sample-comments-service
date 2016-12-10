68030de94eb25236a3499242455202eba204694b
Vincent Dubois <dubois.vct@free.fr>
2016-12-10 18:02:36 +0100

    [FIX] All poms and autoconfigurations

--- src/main/java/io/github/vdubois/SpringCloudSampleCommentsServiceApplication.java
+++ src/main/java/io/github/vdubois/SpringCloudSampleCommentsServiceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCircuitBreaker
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableCaching
 public class SpringCloudSampleCommentsServiceApplication {
 
